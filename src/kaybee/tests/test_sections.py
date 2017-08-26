51d74e8d5272704dad22e3717d7bf4e33601440c
Paul Everitt <pauleveritt@me.com>
2017-08-26 11:32:04 -0400

    Move to fixtures.

--- src/kaybee/tests/test_sections.py
+++ src/kaybee/tests/test_sections.py
@@ -1,16 +1,20 @@
 '''
 To do
-- Make fixtures
-- Get some coverage
+- Rename config to be out from html context and into kb.
 - Rename the config to be more section-focused
 - Perhaps have something different for nav
 - Get the homepage style/template into config
+- Homepage logo into config
 - Fix sectionpage_template vs. template, use listing/doc in the name
 - Figure out "active"
 '''
 
+import pytest
+from kaybee import choose_layout_info
 
-def dummy_sections():
+
+@pytest.fixture
+def sections():
     return [
         dict(path="blog", title="Blog", href="/blog/",
              color='warning'),
@@ -23,95 +27,74 @@ def dummy_sections():
     ]
 
 
-def test_homepage():
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
+def test_homepage(sections):
     pagename = 'index'
     li = choose_layout_info(sections, pagename)
     assert li['style'] == 'header-image is-medium'
     assert li['template'] == 'homepage'
 
 
-def test_homepage_override_template():
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
+def test_homepage_override_template(sections):
     pagename = 'index'
     li = choose_layout_info(sections, pagename, 'customtemplate')
     assert li['style'] == 'header-image is-medium'
     assert li['template'] == 'customtemplate'
 
 
-def test_listing():
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
+def test_listing(sections):
     pagename = 'blog/index'
     li = choose_layout_info(sections, pagename)
     assert li['style'] == 'is-bold is-warning'
     assert li['template'] == 'sectionpage'
 
 
-def test_listing_sectionoverride_template():
+def test_listing_sectionoverride_template(sections):
     # config sections override the template for the listing
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
     pagename = 'articles/index'
     li = choose_layout_info(sections, pagename)
     assert li['style'] == 'is-bold is-warning'
     assert li['template'] == 'articlestemplate'
 
 
-def test_listing_section_and_doc_override_template():
+def test_listing_section_and_doc_override_template(sections):
     # config sections override the template for the listing, but
     # then the doc itself overrides
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
     pagename = 'articles/index'
     li = choose_layout_info(sections, pagename, 'customtemplate')
     assert li['style'] == 'is-bold is-warning'
     assert li['template'] == 'customtemplate'
 
 
-def test_listing_docoverride_template():
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
+def test_listing_docoverride_template(sections):
     pagename = 'blog/index'
     li = choose_layout_info(sections, pagename, 'customtemplate')
     assert li['style'] == 'is-bold is-warning'
     assert li['template'] == 'customtemplate'
 
 
-def test_leaf_default():
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
+def test_leaf_default(sections):
     pagename = 'blog/firstpost'
     li = choose_layout_info(sections, pagename)
     assert li['style'] == 'is-bold is-warning'
     assert li['template'] == 'page'
 
 
-def test_leaf_override_template():
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
+def test_leaf_override_template(sections):
     pagename = 'blog/firstpost'
     li = choose_layout_info(sections, pagename, 'customtemplate')
     assert li['style'] == 'is-bold is-warning'
     assert li['template'] == 'customtemplate'
 
 
-def test_leaf_sectionoverride():
+def test_leaf_sectionoverride(sections):
     # The sectiondef overrides the template
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
     pagename = 'articles/firstpost'
     li = choose_layout_info(sections, pagename)
     assert li['style'] == 'is-bold is-warning'
     assert li['template'] == 'articletemplate'
 
 
-def test_leaf_sectionoverride_docoverrid():
-    # The section overrides, then the document leaf overrides
-    from kaybee import choose_layout_info
-    sections = dummy_sections()
+def test_leaf_sectionoverride_docoverrid(sections):
     pagename = 'blog/firstpost'
     li = choose_layout_info(sections, pagename, 'customtemplate')
     assert li['style'] == 'is-bold is-warning'
