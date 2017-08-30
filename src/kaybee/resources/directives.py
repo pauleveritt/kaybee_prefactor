02ac4b61b51525ac338a2ff33ad8e7a3e6c2cfc3
Paul Everitt <pauleveritt@me.com>
2017-08-30 07:55:04 -0400

    Add BaseDirective

---
+++ src/kaybee/resources/directives.py
@@ -0,0 +1,21 @@
+from docutils.parsers.rst import Directive
+
+
+class ResourceDirective(Directive):
+    has_content = True
+    required_arguments = 1
+
+    def run(self):
+        env = self.state.document.settings.env
+        rtype = self.arguments[0]
+        # There has to be a better way than this. Requires that
+        # the directive be placed after the rst title
+        title = self.state.parent.parent.children[0].children[0].rawsource
+        block = '\n'.join(self.content)
+        klass = env.site.getClass(rtype)
+        resource = klass(env.docname, rtype, title, block)
+
+        # TODO If the config says to validate, validate
+        resource.validate(resource.props, resource.schema)
+        env.site.add(resource)
+        return []
---
+++ src/kaybee/resources/tests/test_directive.py
@@ -0,0 +1,11 @@
+from resources.directives import ResourceDirective
+
+
+def test_import():
+    assert ResourceDirective.__name__ == 'ResourceDirective'
+
+
+def test_class_defaults():
+    assert ResourceDirective.has_content is True
+    assert ResourceDirective.required_arguments == 1
+
