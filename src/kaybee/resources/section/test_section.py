a818a484fbc2b8e814621ea82ec43614d410e501
Paul Everitt <pauleveritt@me.com>
2017-08-31 07:44:08 -0400

    Add some more tests.

--- src/kaybee/resources/section/test_section.py
+++ src/kaybee/resources/section/test_section.py
@@ -1,39 +1,53 @@
+from pykwalify.errors import SchemaError
 import pytest
 from kaybee.resources.section import Section
 
 LOAD = 'kaybee.resources.base_resource.BaseResource.load'
-PAGENAME = 'articles/index'
-RTYPE = 'article'
-TITLE = 'These Are Articles'
+PAGENAME = 'tutorials/index'
+RTYPE = 'section'
+TITLE = 'These Are Tutorials'
 
 
 def test_import():
     assert Section.__name__ == 'Section'
 
 
-def test_construct(monkeypatch):
-    # monkeypatch.setattr(LOAD, lambda c: dict(x=1))
-    content = """
-style: ''
-sort: 90
-is_home: true    
-    """
-    c1 = """
-
-    
-    """
-    s = Section(PAGENAME, RTYPE, TITLE, c1)
+def test_construction(monkeypatch):
+    monkeypatch.setattr(LOAD, lambda c: dict(style='s1'))
+    s = Section(PAGENAME, RTYPE, TITLE, '')
     s.validate(s.props, s.schema)
-    assert s.props['x'] == 1
+    assert s.props['style'] == 's1'
+    assert s.name == 'tutorials'
+    assert s.rtype == 'section'
+    assert s.title == TITLE
 
 
-@pytest.mark.skip(reason="no way of currently testing this")
-def test_construct2(monkeypatch):
-    monkeypatch.setattr(LOAD, lambda c: dict(x=2))
+def test_has_props(monkeypatch):
     content = """
-style: ''
+style: is-medium
 sort: 90
 is_home: True    
     """
     s = Section(PAGENAME, RTYPE, TITLE, content)
-    assert s.props['x'] == 2
+    assert s.props['style'] == 'is-medium'
+    assert s.props['sort'] == 90
+    assert s.props['is_home'] is True
+
+
+def test_is_valid(monkeypatch):
+    content = """
+style: is-medium
+sort: 90
+is_home: True    
+    """
+    s = Section(PAGENAME, RTYPE, TITLE, content)
+    s.validate(s.props, s.schema)
+
+
+def test_is_invalid(monkeypatch):
+    content = """
+xstyle: is-medium
+    """
+    s = Section(PAGENAME, RTYPE, TITLE, content)
+    with pytest.raises(SchemaError):
+        s.validate(s.props, s.schema)
