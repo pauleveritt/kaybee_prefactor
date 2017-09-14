58b9fdb813e7c106fc54df560f250c2f3a0fb10d
Paul Everitt <pauleveritt@me.com>
2017-09-14 19:26:28 -0400

    Improve some tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,6 @@ from sphinx.testing.path import path
 pytest_plugins = 'sphinx.testing.fixtures'
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='module')
 def rootdir():
     return path(os.path.dirname(__file__) or '.').abspath() / 'roots'
