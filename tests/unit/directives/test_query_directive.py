4af2539c6232b4f697d396d59cb0619575a555a9
Paul Everitt <pauleveritt@me.com>
2017-09-20 16:43:50 -0400

    Add a test for the query Node.

--- tests/unit/directives/test_query_directive.py
+++ tests/unit/directives/test_query_directive.py
@@ -1,47 +1,11 @@
-from kaybee.directives import QueryDirective, query
-
-LOAD = 'kaybee.directives.QueryDirective.load'
+from kaybee.directives.query import QueryDirective
 
 
-def test_qd_import():
+def test_import():
     assert QueryDirective.__name__ == 'QueryDirective'
 
 
-def test_qn_import():
-    assert query.__name__ == 'QueryNode'
-
-
-def test_qn_construction():
-    qn = query()
-    assert qn.__class__.__name__ == 'QueryNode'
-
-
-def test_qd_class_defaults():
+def test_class_defaults():
     assert QueryDirective.has_content is True
+    assert QueryDirective.required_arguments == 1
 
-
-def test_qd_yaml_load_empty():
-    content = """
-    
-    
-    """
-    props = QueryDirective.load(content)
-    assert props == dict()
-
-
-def test_qd_package_dir():
-    assert QueryDirective.package_dir().endswith('kaybee/directives')
-
-
-def test_qd_yaml_load():
-    content = """
-flag1: 1    
-flag2: 2    
-    """
-    props = QueryDirective.load(content)
-    assert props == dict(flag1=1, flag2=2)
-
-#
-# Giving up at this point, contructing a Directive is
-# quite painful. Switch to integration tests.
-#
---
+++ tests/unit/directives/test_query_node.py
@@ -0,0 +1,5 @@
+from kaybee.directives.query import query
+
+
+def test_import():
+    assert query.__name__ == 'query'
