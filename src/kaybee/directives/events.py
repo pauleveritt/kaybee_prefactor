b1b3046b010b07fd853b6bf9ac2ac87fcffaf3eb
Paul Everitt <pauleveritt@me.com>
2017-09-21 13:27:33 -0400

    Move the process query nodes event handler to its own file.

---
+++ src/kaybee/directives/events.py
@@ -0,0 +1,23 @@
+from docutils import nodes
+from sphinx.application import Sphinx
+from sphinx.builders.html import StandaloneHTMLBuilder
+
+from kaybee.directives.query import query
+
+
+def process_query_nodes(app: Sphinx, doctree, fromdocname):
+    """ Callback registered with Sphinx's doctree-resolved event """
+    # Setup a template and context
+    builder: StandaloneHTMLBuilder = app.builder
+    site = app.env.site
+
+    for node in doctree.traverse(query):
+        # Render the output
+        widget = site.widgets.get(node.name)
+        context = builder.globalcontext.copy()
+        context['site'] = site
+        output = widget.render(builder, context, site)
+
+        # Put the output into the node contents
+        listing = [nodes.raw('', output, format='html')]
+        node.replace_self(listing)
