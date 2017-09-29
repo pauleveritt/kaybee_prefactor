6b8a02da073c802a9bd35e5d3ce4d6dfd56c77ee
Paul Everitt <pauleveritt@me.com>
2017-09-29 18:15:11 -0400

    Re-organize core events testing.

--- tests/unit/setup/test_kb_context.py
+++
@@ -1,9 +0,0 @@
-"""
-Test the html context event handler
-"""
-
-from kaybee.events import kb_context
-
-
-def test_import():
-    assert kb_context.__name__ == 'kb_context'
---
+++ tests/unit/test_events.py
@@ -0,0 +1,113 @@
+"""
+Test the html context event handler
+"""
+import pytest
+
+from kaybee.events import (
+    initialize_site, kb_context, purge_resources
+)
+
+
+class DummyResource:
+    title = 'doc1 is here'
+
+    def __init__(self):
+        self.name = 'doc1'
+
+    def parents(self, site):
+        return [1, 2, 3]
+
+    def template(self, site):
+        return 'dummyresource.html'
+
+
+class DummyConfig:
+    html_context = dict(flag=9)
+
+
+class DummyEnv:
+    pass
+
+
+class DummySite:
+    def __init__(self):
+        self.resources = dict(doc1='doc1')
+
+    def remove_resource(self, docname):
+        self.resources.pop(docname, None)
+
+
+class DummyApp:
+    def __init__(self):
+        self.config = DummyConfig()
+        self.env = DummyEnv()
+
+
+@pytest.fixture(name='resource')
+def dummy_resource():
+    yield DummyResource()
+
+
+@pytest.fixture(name='app')
+def dummy_app():
+    yield DummyApp()
+
+
+@pytest.fixture(name='env')
+def dummy_env():
+    yield DummyEnv()
+
+
+@pytest.fixture(name='site')
+def dummy_site(resource):
+    ds = DummySite()
+    ds.resources[resource.name] = resource
+    yield ds
+
+
+class TestInitializeSite:
+    def test_import(self):
+        assert initialize_site.__name__ == 'initialize_site'
+
+    def test_setup(self, app, env):
+        initialize_site(app, env, [])
+        assert env.site.config['flag'] == 9
+
+
+class TestPurgeResources:
+    def test_import(self):
+        assert purge_resources.__name__ == 'purge_resources'
+
+    def test_has_site(self, app, env, site):
+        env.site = site
+        assert 'doc1' in env.site.resources
+        purge_resources(app, env, 'doc1')
+        assert 'doc1' not in env.site.resources
+
+
+class TestKbContext:
+    def test_import(self):
+        assert kb_context.__name__ == 'kb_context'
+
+    def test_no_resource(self, app, site):
+        app.env.site = site
+        pagename = ''
+        templatename = 'defaulttemplate'
+        context = dict()
+        doctree = None
+        result = kb_context(app, pagename, templatename, context, doctree)
+        assert result == templatename
+
+    def test_resource(self, app, site, resource):
+        app.env.site = site
+        pagename = 'doc1'
+        templatename = 'defaulttemplate'
+        context = dict()
+        doctree = None
+        result = kb_context(app, pagename, templatename, context, doctree)
+        assert result == 'dummyresource.html'
+        assert context['template'] == 'dummyresource.html'
+        assert context['site'] == site
+        assert context['resource'] == resource
+        assert context['parents'] == [1, 2, 3]
+        assert context['title'] == 'doc1 is here'
