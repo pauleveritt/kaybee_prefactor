ca66fef95a01bb08d29439b8a533fcee0fcb95a1
Paul Everitt <pauleveritt@me.com>
2017-10-02 18:55:03 -0400

    Extract to a new registry module.

---
+++ src/kaybee/core/registry.py
@@ -0,0 +1,85 @@
+"""
+
+Use Dectate to make kaybee decorators for configuration.
+
+We need a simple plugin mechanism, to let resource and widget
+definitions come from (a) kaybee, (b) other installed packages,
+and (c) people writing a Sphinx site that do simple extensions.
+We'll use Dectate for that.
+
+This registration system provides the following services:
+
+- Register a type of widget/resource for Sphinx node/directive
+
+- Register the class that will be injected
+
+- Register the schema used to validate the YAML
+
+"""
+import dectate
+
+
+class SiteAction(dectate.Action):
+    config = {
+        'site': dict
+    }
+
+    def __init__(self):
+        super().__init__()
+        self.name = 'singleton'
+
+    def identifier(self, site):
+        return self.name
+
+    def perform(self, obj, site):
+        site[self.name] = obj
+
+
+class ResourceAction(dectate.Action):
+    config = {
+        'resources': dict
+    }
+
+    def __init__(self, name, defaults=None, references=None):
+        super().__init__()
+        self.name = name
+        self.defaults = defaults
+        self.references = references
+
+    def identifier(self, resources):
+        return self.name
+
+    def perform(self, obj, resources):
+        resources[self.name] = obj
+
+
+class WidgetAction(dectate.Action):
+    config = {
+        'widgets': dict
+    }
+
+    def __init__(self, name, defaults=None, references=None):
+        super().__init__()
+        self.name = name
+        self.defaults = defaults
+        self.references = references
+
+    def identifier(self, widgets):
+        return self.name
+
+    def perform(self, obj, widgets):
+        widgets[self.name] = obj
+
+
+class registry(dectate.App):
+    widget = dectate.directive(WidgetAction)
+    resource = dectate.directive(ResourceAction)
+    site = dectate.directive(SiteAction)
+
+    @classmethod
+    def first_action(cls, kind, kbtype):
+        qr = dectate.Query(kind)
+        # Use a generator expression to get the first action
+        # in "kind" with a name of ktype. kind might be
+        # "resource" or "widget", kbtype might be "article".
+        return next((x for x in qr.filter(name=kbtype)(cls)))[0]
---
+++ tests/unit/core/test_registry.py
@@ -0,0 +1,96 @@
+import dectate
+import pytest
+
+from kaybee.core.registry import ResourceAction, WidgetAction
+
+
+class DummySection:
+    pass
+
+
+@pytest.fixture()
+def registry():
+    class registry(dectate.App):
+        dummyresource = dectate.directive(ResourceAction)
+        dummywidget = dectate.directive(WidgetAction)
+
+        @classmethod
+        def first_action(cls, kind, kbtype):
+            qr = dectate.Query(kind)
+            return next((x for x in qr.filter(name=kbtype)(cls)))[0]
+
+    yield registry
+
+
+@pytest.fixture()
+def register_article(registry):
+    @registry.dummyresource('dummyarticle', defaults=dict(x=1),
+                            references=[1, 3])
+    class DummyArticle:
+        pass
+
+    yield DummyArticle
+
+
+@pytest.fixture()
+def query_resource(registry, register_article):
+    yield dectate.Query('dummyresource')
+
+
+class TestRegistry:
+    def test_construction(self, registry, query_resource):
+        dectate.commit(registry)
+
+        results = list(query_resource(registry))
+        assert len(results) == 1
+
+    def test_clears_registray_second_time(self, registry, query_resource):
+        with pytest.raises(AttributeError):
+            list(query_resource(registry))
+
+    def test_find_by_type(self, registry, query_resource):
+        dectate.commit(registry)
+        da = registry.first_action('dummyresource', 'dummyarticle')
+        assert da.__class__.__name__.endswith('ResourceAction')
+
+    def test_type_defaults(self, registry, query_resource):
+        dectate.commit(registry)
+        da = registry.first_action('dummyresource', 'dummyarticle')
+        assert da.defaults['x'] == 1
+
+    def test_type_no_defaults(self, registry):
+        registry.dummyresource('dummysection')(DummySection)
+        dectate.commit(registry)
+        ds = registry.first_action('dummyresource', 'dummysection')
+        assert ds.defaults is None
+
+    def test_type_references(self, registry, query_resource):
+        dectate.commit(registry)
+        da = registry.first_action('dummyresource', 'dummyarticle')
+        assert da.references == [1, 3]
+
+    def test_type_no_references(self, registry):
+        registry.dummyresource('dummysection')(DummySection)
+        dectate.commit(registry)
+        da = registry.first_action('dummyresource', 'dummysection')
+        assert da.references is None
+
+    def test_imperative_add(self, registry):
+        # This is how YAML-defined types will add type info
+        registry.dummyresource('dummysection')(DummySection)
+        dectate.commit(registry)
+        assert registry.config.resources['dummysection'] == DummySection
+
+    def test_imperative_add_defaults_references(self, registry):
+        # This is how YAML-defined types will add type info
+        d = dict(x=99)
+        r = [1, 3]
+        registry.dummyresource('dummysection',
+                               defaults=d, references=r)(DummySection)
+        dectate.commit(registry)
+        ds = registry.first_action('dummyresource', 'dummysection')
+        assert ds.defaults['x'] == 99
+        assert ds.references == [1, 3]
+
+# TODO
+# Test a YAML typedef getting associated with a registered class
