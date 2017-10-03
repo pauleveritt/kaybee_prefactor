86ece712c806ef4834c90e95c972b52b26340e63
Paul Everitt <pauleveritt@me.com>
2017-10-03 10:18:01 -0400

    Basic read/load/validate is tested.

--- src/kaybee/core/typedefs.py
+++ src/kaybee/core/typedefs.py
@@ -1,15 +1,7 @@
-""" Centralize definition of types
-
-You can register stuff in kaybee in several ways: YAML in the
-docs project's conf area, classes in the docs project's conf area,
-and of course kaybee's builtin types (article, section, homepage, etc.)
-
-We need to route the discovery and validation through some common
-policies.
-
+""" Extract typedef info from YAML and register
  """
 import inspect
-from typing import Dict
+from typing import Dict, Sequence
 
 import os
 
@@ -25,25 +17,58 @@ typeinfo_path = os.path.join(
 
 
 class KbTypedefDocsError(Exception):
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         Exception.__init__(self, 'typedef files need two YAML documents')
 
 
-class KbTypedef:
+class YamlTypedef:
     kind: str
-    name: str
+    kbtype: str
+    defaults: Dict
+    references: Sequence
     schema: Dict
 
-    def __init__(self, kind='', name='', yaml_fn=None):
-        if yaml_fn:
-            with open(yaml_fn, 'r') as f:
-                content = f.read()
-                docs = list(load_all(content))
-                if len(docs) != 2:
-                    raise KbTypedefDocsError()
-                typeinfo, schema = docs
+    def read(self):
+        with open(self.yaml_fn) as f:
+            self.yaml_content = f.read()
 
-                # Validate the typedef
-                c = Core(source_data=typeinfo, schema_files=[typeinfo_path])
-                c.validate(raise_exception=True)
+    def load(self):
+        docs = list(load_all(self.yaml_content))
+        if len(docs) != 2:
+            raise KbTypedefDocsError()
+        self.typeinfo, self.schema = docs
 
+    def validate(self):
+        c = Core(source_data=self.typeinfo, schema_files=[typeinfo_path])
+        c.validate(raise_exception=True)
+
+    def __init__(self, yaml_fn=None):
+        self.yaml_fn = yaml_fn
+        self.yaml_content = None
+        self.typeinfo = None
+        self.schema = None
+
+        # Now do each phase of processing
+        self.read()
+        self.load()
+        self.validate()
+
+    @property
+    def kind(self):
+        return self.typeinfo['kind']
+
+    @property
+    def kbtype(self):
+        return self.typeinfo['kbtype']
+
+    @property
+    def defaults(self):
+        return self.typeinfo.get('defaults', dict())
+
+    @property
+    def references(self):
+        return self.typeinfo.get('references', [])
+
+    @property
+    def klass(self):
+        return self.typeinfo['kind']
--- tests/unit/core/test_typedefs.py
+++ tests/unit/core/test_typedefs.py
@@ -1,12 +1,17 @@
 import inspect
 import os
 
-import dectate as dectate
-import pytest as pytest
+import pytest
 from pykwalify.errors import SchemaError
 from ruamel.yaml.scanner import ScannerError
 
-from kaybee.core.typedefs import KbTypedef, KbTypedefDocsError
+from kaybee.core.typedefs import YamlTypedef, KbTypedefDocsError
+
+YAMLTYPEDEF_PATH = 'kaybee.core.typedefs.YamlTypedef'
+LOAD = YAMLTYPEDEF_PATH + '.load'
+READ = YAMLTYPEDEF_PATH + '.read'
+VALIDATE = YAMLTYPEDEF_PATH + '.validate'
+skip = lambda x: None
 
 
 def make_yaml_filename(fn):
@@ -19,21 +24,49 @@ def make_yaml_filename(fn):
     )
 
 
-class TestKbTypedf:
+class TestYamlTypedf:
     def test_import(self):
-        assert KbTypedef.__name__ == 'KbTypedef'
+        assert YamlTypedef.__name__ == 'YamlTypedef'
 
-    def test_exception_not_well_formed(self):
+    def test_construction(self, monkeypatch):
+        monkeypatch.setattr(LOAD, skip)
+        monkeypatch.setattr(READ, skip)
+        monkeypatch.setattr(VALIDATE, skip)
+        fn = make_yaml_filename('valid_article.yaml')
+        yaml_typedef = YamlTypedef(fn)
+
+        assert yaml_typedef.__class__.__name__ == 'YamlTypedef'
+        assert yaml_typedef.yaml_fn.endswith('valid_article.yaml')
+        assert yaml_typedef.yaml_content is None
+        assert yaml_typedef.typeinfo is None
+        assert yaml_typedef.schema is None
+
+    def test_read(self, monkeypatch):
+        monkeypatch.setattr(VALIDATE, skip)
+        fn = make_yaml_filename('valid_article.yaml')
+        yaml_typedef = YamlTypedef(fn)
+        header = '# Well-formed AND valid YAML'
+        assert yaml_typedef.yaml_content.startswith(header)
+
+    def test_load_successfully(self, monkeypatch):
+        monkeypatch.setattr(VALIDATE, skip)
+        fn = make_yaml_filename('valid_article.yaml')
+        yaml_typedef = YamlTypedef(fn)
+        assert len(yaml_typedef.typeinfo.keys())
+        assert len(yaml_typedef.schema.keys())
+
+    def test_exception_not_well_formed(self, monkeypatch):
         yaml_fn = make_yaml_filename('not_well_formed_article.yaml')
         with pytest.raises(ScannerError):
-            KbTypedef(yaml_fn=yaml_fn)
+            yaml_typedef = YamlTypedef(yaml_fn)
 
-    def test_exception_not_two_docs(self):
+    def test_exception_not_two_docs(self, monkeypatch):
         yaml_fn = make_yaml_filename('not_two_docs.yaml')
         with pytest.raises(KbTypedefDocsError):
-            KbTypedef(yaml_fn=yaml_fn)
+            yaml_typedef = YamlTypedef(yaml_fn)
 
-    def test_exception_not_valid_typedef(self):
+    def test_exception_not_valid_typedef(self, monkeypatch):
         yaml_fn = make_yaml_filename('not_valid_article.yaml')
         with pytest.raises(SchemaError):
-            KbTypedef(yaml_fn=yaml_fn)
+            YamlTypedef(yaml_fn=yaml_fn)
+            yaml_typedef = YamlTypedef(yaml_fn)
