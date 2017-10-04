f243118a5e8289336376e20347268e8399e335fa
Paul Everitt <pauleveritt@me.com>
2017-10-04 09:29:01 -0400

    Prep for theme options.

---
+++ tests/integration/roots/test-theme-options/conf.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import kaybee
+
+extensions = [kaybee.__title__]
+
+master_doc = 'index'
+html_theme = 'kaybee'
+exclude_patterns = ['_build']
+
+alt = 'Kaybee Logo Alt'
+html_context = dict(
+    kaybee_config={},
+    logo=dict(
+        img_url='http://some.site.com/fake_image.png',
+        alt=alt
+    ),
+    social_media=dict(
+        twitter='paulweveritt',
+        github='pauleveritt'
+    )
+)
