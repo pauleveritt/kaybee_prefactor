c6fd58c8e509989ecff76eb2138921c30ce09e9a
Paul Everitt <pauleveritt@me.com>
2017-10-16 14:45:29 -0400

    An attempt at unit testing, but wow, deeply-nested stuff.

---
+++ tests/unit/widgets/test_widget_events.py
@@ -0,0 +1,36 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from kaybee.widgets.events import process_widget_nodes
+
+
+@pytest.fixture()
+def app(mocker):
+    app = MagicMock()
+
+    yield app
+
+
+@pytest.fixture()
+def nodes(monkeypatch):
+    node1 = MagicMock()
+    node1.attributes = dict(
+        hidden=0,
+        entries=[(None, 'articles/article1')]
+    )
+    yield [node1, ]
+
+
+@pytest.fixture()
+def doctree(monkeypatch, nodes):
+    doctree = MagicMock()
+    monkeypatch.setattr(doctree, 'traverse', lambda x: nodes)
+
+    yield doctree
+
+
+class TestWidgetEvents:
+    def test_process_widget_nodes(self, app, doctree, nodes):
+        process_widget_nodes(app, doctree, 'fromdocname22')
+        assert 1 == 2
