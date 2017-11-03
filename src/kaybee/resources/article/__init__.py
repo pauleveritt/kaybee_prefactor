82881ee40d69be0c3e9799d34b9235400d30876d
Paul Everitt <pauleveritt@me.com>
2017-11-03 14:57:28 -0400

    Don't put a non-resource toctree entry into the series.

--- src/kaybee/resources/article/__init__.py
+++ src/kaybee/resources/article/__init__.py
@@ -15,12 +15,15 @@ class Article(BaseResource):
         parent = site.resources[self.parent]
         results = []
         for docname in parent.toctree:
-            resource = site.resources[docname]
-            results.append(
-                dict(
-                    docname=docname,
-                    title=resource.title,
-                    synopsis=resource.props.synopsis
+            resource = site.resources.get(docname)
+            if resource:
+                # We might have a non-resource page in the toctree,
+                # so skip it if true
+                results.append(
+                    dict(
+                        docname=docname,
+                        title=resource.title,
+                        synopsis=resource.props.synopsis
+                    )
                 )
-            )
         return results
