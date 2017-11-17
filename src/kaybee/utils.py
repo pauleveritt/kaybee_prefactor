f54599cb04fa8cf93f8ebf759613ec96f3bf2923
Paul Everitt <pauleveritt@me.com>
2017-11-17 17:01:23 -0500

    Documents, titles, and excerpts.

--- src/kaybee/utils.py
+++ src/kaybee/utils.py
@@ -1,7 +1,21 @@
 """
 Miscellaneous functions used in Kaybee.
 """
+from docutils import nodes
 from docutils.core import publish_parts
+from docutils.frontend import OptionParser
+from docutils.nodes import Node, document, paragraph
+from docutils.parsers.rst import Parser
+from docutils.utils import new_document
+
+
+def rst_document(rst_string: str) -> document:
+    default_settings = OptionParser(
+        components=(Parser,)).get_default_values()
+    document = new_document(rst_string, default_settings)
+    parser = Parser()
+    parser.parse(rst_string, document)
+    return document
 
 
 def rst_to_html(input_string: str) -> str:
@@ -15,3 +29,21 @@ def rst_to_html(input_string: str) -> str:
         settings_overrides=overrides
     )
     return parts['html_body']
+
+
+def get_rst_title(rst_doc: Node) -> str:
+    """ Given some RST, extract what docutils thinks is the title """
+
+    for title in rst_doc.traverse(nodes.title):
+        return title.astext()
+
+
+def get_rst_excerpt(rst_doc: document, paragraphs: int = 1) -> str:
+    """ Given rst, parse and return a portion """
+
+    texts = []
+    for count, p in enumerate(rst_doc.traverse(paragraph)):
+        texts.append(p.astext())
+        if count + 1 == paragraphs:
+            break
+    return ' '.join(texts)
--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -1,4 +1,15 @@
-from kaybee.utils import rst_to_html
+import pytest
+
+from kaybee.utils import (
+    rst_to_html, get_rst_excerpt, get_rst_title,
+    rst_document
+)
+
+
+def test_rst_document():
+    source = 'Hello *world*'
+    result = rst_document(source)
+    assert 'document' == result.__class__.__name__
 
 
 def test_rst_to_html():
@@ -7,3 +18,47 @@ def test_rst_to_html():
     assert '<div class="document">' in result
     assert '<p>Hello <em>world</em></p>' in result
     assert '</div>' in result
+
+
+def test_simple():
+    source = """
+Test *Simple*
+=============
+
+Body       
+    
+    """
+    node = rst_document(source)
+    result = get_rst_title(node)
+    assert 'Test Simple' == result
+
+
+@pytest.fixture()
+def excerpt():
+    source = """
+Test
+====
+
+First *paragraph*.
+
+Second *paragraph*.        
+            """
+    yield rst_document(source)
+
+
+class TestGetRstExcerpt:
+    def test_default(self, excerpt):
+        """ By default, use the first paragraph """
+        result = get_rst_excerpt(excerpt)
+        assert 'First paragraph.' == result
+
+    def test_multiple_paragraphs(self, excerpt):
+        """ By configuration, you can ask for more paragraphs """
+
+        result = get_rst_excerpt(excerpt, 2)
+        assert 'First paragraph. Second paragraph.' == result
+
+# Other excerpt policies, outside of RST extraction:
+# - exclude_excerpt, on a single resource
+# - exclude_excerpt site-wide
+# - Getting the value from synopsis in the model
